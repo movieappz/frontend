62565381765a0f586b90e78bb008620a0ff710e2
Farid Tavakoli <89019161+FaridTvK@users.noreply.github.com>
2025-09-23 15:26:23 +0200

    search function

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,25 +1,35 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router";
 import { mainContext, type MainContextProps } from "../../context/MainProvider";
 
 export default function Search() {
+  const navigate = useNavigate();
   const { searchRef, searchMovies } = useContext(
     mainContext
   ) as MainContextProps;
 
-  // useEffect(() => {
-  //   console.log(searchRef.current?.value);
-  //   searchMovies(searchRef.current?.value);
-  // }, []);
+  const handleChange = async () => {
+    const val = (searchRef.current?.value || "").trim();
+    if (val.length > 0) {
+      await searchMovies(val);
+      setTimeout(() => {
+        if (searchRef?.current?.value === "") {
+          window.location.reload();
+          navigate("/category/28");
+        }
+      }, 5000);
+    } else {
+      navigate("/category/28");
+    }
+  };
 
   return (
-    <>
-      <input
-        type="text"
-        ref={searchRef}
-        onChange={() => searchMovies(searchRef.current?.value)}
-        placeholder="search Movie..."
-        className="w-full sm:w-64 bg-white/90 text-[--color-brand-text] placeholder:text-[--color-brand-text]/60 border-2 border-[--color-brand-border] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[--color-brand-border]"
-      />
-    </>
+    <input
+      type="text"
+      ref={searchRef}
+      onChange={handleChange}
+      placeholder="search Movie..."
+      className="w-full sm:w-64 bg-white/90 text-[--color-brand-text] placeholder:text-[--color-brand-text]/60 border-2 border-[--color-brand-border] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[--color-brand-border]"
+    />
   );
 }
