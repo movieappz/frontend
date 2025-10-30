c336189c142644eeeca36832d94e7a61be875d7d
Farid Tavakoli <89019161+FaridTvK@users.noreply.github.com>
2025-10-30 14:58:30 +0100

    frontend done

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:2000";
+const baseURL = import.meta.env.VITE_API_URL;
 
 export const axiosPublic = axios.create({
     baseURL,
