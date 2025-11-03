d5e8623de7555d5412f5ea835e9eb7ec60651936
Farid Tavakoli <89019161+FaridTvK@users.noreply.github.com>
2025-11-03 14:41:57 +0100

    frontend fixed v1

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
 
   const handleLogout = async () => {
     try {
-      await axiosPublic.post("/logout", {}, { withCredentials: true });
+      await axiosPublic.post("/auth/logout", {}, { withCredentials: true });
     } catch (error) {
       console.error("Logout error:", error);
     } finally {
